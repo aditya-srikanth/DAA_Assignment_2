bfc32e4fb36f962b7c0d4608af3faf428f6f0b0b
tanmay_kulkarni <tanmaygk97@gmail.com>
2019-04-03 09:41:56 +0530

    add init of algo

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@ int main(){
 	std::vector<Node> values = data.read_data();
 	// TODO
 	//Algorithm comes here
+	double slope_of_best_fit,intercept_of_best_fit,sum_of_x,sum_of_y,sum_of_x_square,sum_of_xy;
+
+	for(int j = 0; j<values.size();j++){
+		slope_of_best_fit = 0 ;
+		intercept_of_best_fit = 0;
+		sum_of_x,sum_of_y = 0;
+		sum_of_x_square,sum_of_xy = 0 ;
+		for(int i=0;i<=j;i++){
+			sum_of_y += values[i].getY();
+			sum_of_x += values[i].getX();
+			sum_of_xy += values[i].getX() * values[i].getY() ;
+			sum_of_x_square += values[i].getX() * values[i].getX();
+ 		}
+			slope_of_best_fit = ( values.size()*sum_of_xy - sum_of_x*sum_of_y ) / (values.size()*sum_of_x_square - sum_of_x*sum_of_x); 
+	}
 	return 0;
 }
 
