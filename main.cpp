95600a5ee7a52118e21f463cf5161ef9044a85ce
tanmay_kulkarni <tanmaygk97@gmail.com>
2019-04-03 09:51:32 +0530

    Make dp table of regression error

--- main.cpp
+++ main.cpp
@@ -8,25 +8,33 @@ int main(){
 	// TODO
 	//Algorithm comes here
 	double slope_of_best_fit,intercept_of_best_fit,sum_of_x,sum_of_y,sum_of_x_square,sum_of_xy;
+	double errors[va.size()][values.size()];
 
-	for(int j = 0; j<values.size();j++){
-		slope_of_best_fit = 0 ;
-		intercept_of_best_fit = 0;
-		sum_of_x,sum_of_y = 0;
-		sum_of_x_square,sum_of_xy = 0 ;
-		for(int i=0;i<=j;i++){
-			sum_of_y += values[i].getY();
-			sum_of_x += values[i].getX();
-			sum_of_xy += values[i].getX() * values[i].getY() ;
-			sum_of_x_square += values[i].getX() * values[i].getX();
- 		}
-			slope_of_best_fit = ( values.size()*sum_of_xy - sum_of_x*sum_of_y ) / (values.size()*sum_of_x_square - sum_of_x*sum_of_x); 
+	for(int k = 0; k < values.size() ; k++){
+		for(int j = 0; j <= k;j++){
+			slope_of_best_fit = 0 ;
+			intercept_of_best_fit = 0;
+			sum_of_x,sum_of_y = 0;
+			sum_of_x_square,sum_of_xy = 0 ;
+			if(j != k){
+				for(int i=j;i<=k;i++){
+					sum_of_y += values[i].getY();
+					sum_of_x += values[i].getX();
+					sum_of_xy += values[i].getX() * values[i].getY() ;
+					sum_of_x_square += values[i].getX() * values[i].getX();
+				}
+				slope_of_best_fit = ( values.size()*sum_of_xy - sum_of_x*sum_of_y ) / (values.size()*sum_of_x_square - sum_of_x*sum_of_x); 
+			}
+			else{
+				error[j][j] = 0;
+			}
+		}
 	}
 	return 0;
 }
 
-double error(double slope,Node point){
-	double predict = slope * point.getX();
+double error(double slope,double intercept,Node point){
+	double predict = slope * point.getX() + intercept;
 	double dev = predict - point.getY();
 	return dev*dev;
 }
